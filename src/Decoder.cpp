ade7cfe5b5c8f94fb5746b8778a2e4e7f4856257
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2018-07-02 02:37:17 -0300

    Add files for decoder reading

---
+++ src/Decoder.cpp
@@ -0,0 +1,196 @@
+//
+// Created by gabriel on 7/1/18.
+//
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include "Decoder.h"
+#include "../include/galileo2io.h"
+
+Decoder::Decoder() {
+    uint8_t mode = SPI_MODE_0;
+    uint8_t lsb = 0;
+    uint8_t bpw = 8;
+    uint32_t speed = SPI_FREQUENCY;
+    unsigned char mode_register_0, mode_register_1;
+
+    //open file descriptors
+
+    if ((spi_fd = open("/dev/spidev1.0", O_RDWR)) < 0) {
+        fprintf(stderr, "Failed to open /dev/spidev1.0\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((_ss_fd = open("/sys/class/gpio/gpio10/value", O_WRONLY)) < 0) {
+        fprintf(stderr, "Failed to open /sys/class/gpio/gpio10/value\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //configure spi communication
+
+    if (ioctl(spi_fd, SPI_IOC_WR_MODE, &mode) < 0) {
+        fprintf(stderr, "Failed to set SPI write mode.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(spi_fd, SPI_IOC_WR_LSB_FIRST, &lsb)) {
+        fprintf(stderr, "Failed to set SPI LSB first.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bpw)) {
+        fprintf(stderr, "Failed to set SPI bits per word.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed)) {
+        fprintf(stderr, "Failed to set SPI max speed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //configure mode registers
+
+    mode_register_0 = QUADRX4 | FREE_RUN | DISABLE_INDX | FILTER_2;
+    mode_register_1 = BYTE_2 | EN_CNTR | NO_FLAGS;
+
+    if (write_byte(WRITE_MDR0, mode_register_0) < 0) {
+        fprintf(stderr, "Failed to configure mode register 0\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (write_byte(WRITE_MDR1, mode_register_1) < 0) {
+        fprintf(stderr, "Failed to configure mode register 1\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //clear counting register
+    reset_count();
+}
+
+
+Decoder::~Decoder() {
+    if (close(spi_fd) < 0) {
+        fprintf(stderr, "Failed to close SPI\n");
+    }
+    if (close(_ss_fd) < 0) {
+        fprintf(stderr, "Failed to close #SS\n");
+    }
+}
+
+int Decoder::get_count() {
+    int i;
+    unsigned int count = 0;
+    unsigned char byte = 0;
+    char read_counter_op = READ_CNTR;
+
+    _ss_high();
+    _ss_low();
+
+    //write spi op code to read counter to spi bus
+    if (write(spi_fd, &read_counter_op, sizeof(read_counter_op)) < 0) {
+        fprintf(stderr, "Failed write read counter op code to SPI bus\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //read 16 bits (8 bytes)
+    for (i = 0; i < 2; i++) {
+        //shift 8 bits
+        count = count << 8;
+
+        //read a byte
+        lseek(spi_fd, 0, SEEK_SET);
+        while (read(spi_fd, &byte, sizeof(byte)) != sizeof(byte));
+
+        //compose count from bytes by ORing
+        count = count | byte;
+    }
+
+    _ss_high();
+
+    return count;
+}
+
+int Decoder::reset_count() {
+    unsigned char op_code = CLR_CNTR;
+
+    if (_ss_high() < 0) return -1;
+    if (_ss_low() < 0) return -1;
+
+    //write spi op code to spi bus
+    if (write(spi_fd, &op_code, sizeof(op_code)) < 0) {
+        fprintf(stderr, "Failed reset counter\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (_ss_high() < 0) return -1;
+
+    return 0;
+}
+
+int Decoder::write_byte(unsigned char op_code, unsigned char data) {
+    if (_ss_high() < 0) return -1;
+    if (_ss_low() < 0) return -1;
+
+    //write spi op code to spi bus
+    if (write(spi_fd, &op_code, sizeof(op_code)) < 0) {
+        fprintf(stderr, "Failed write op code to SPI\n");
+        return -1;
+    }
+
+    // send data
+    if (write(spi_fd, &data, sizeof(data)) < 0) {
+        fprintf(stderr, "Failed write data to SPI\n");
+        return -1;
+    }
+
+    if (_ss_high() < 0) return -1;
+
+    return 0;
+}
+
+unsigned char Decoder::read_byte(unsigned char op_code) {
+    unsigned char byte;
+
+    if (_ss_high() < 0) exit(EXIT_FAILURE);
+    if (_ss_low() < 0) exit(EXIT_FAILURE);
+
+    //write spi op code to spi bus
+    if (write(spi_fd, &op_code, sizeof(op_code)) < 0) {
+        fprintf(stderr, "Failed write op code to SPI\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //read data from the spi bus when available
+    lseek(spi_fd, 0, SEEK_SET);
+    while (read(spi_fd, &byte, sizeof(byte)) != sizeof(byte));
+
+    if (_ss_high() < 0) exit(EXIT_FAILURE);
+
+    return byte;
+}
+
+int Decoder::_ss_low() {
+    //go to beginning of file
+    lseek(_ss_fd, 0, SEEK_SET);
+    //write to file and check if success
+    if (write(_ss_fd, "0", sizeof("0")) < 0) {
+        fprintf(stderr, "Failed to set #SS to low\n");
+        return -1;
+    }
+    return 0;
+}
+
+int Decoder::_ss_high() {
+    //go to beginning of file
+    lseek(_ss_fd, 0, SEEK_SET);
+    //write to file and check if success
+    if (write(_ss_fd, "1", sizeof("1")) < 0) {
+        fprintf(stderr, "Failed to set #SS to high\n");
+        return -1;
+    }
+    return 0;
+}
---
+++ src/Decoder.h
@@ -0,0 +1,147 @@
+//
+// Created by gabriel on 7/1/18.
+//
+
+#ifndef QUANSERCONTROLLERSHIELD_DECODER_H
+#define QUANSERCONTROLLERSHIELD_DECODER_H
+
+
+class Decoder {
+public:
+
+    /**
+     * Save SPI
+     * Set slave select
+     * Set CPOL and CPHA
+     */
+    Decoder();
+
+    /**
+     * Close open files and end SPI
+     */
+    virtual ~Decoder();
+
+    /**
+     * Read counting from SPI
+     * @return
+     */
+    int get_count();
+
+private:
+
+    /**
+     * File descriptor for SPI
+     */
+    int spi_fd;
+
+    /**
+     * File descriptor for #SS
+     */
+    int _ss_fd;
+
+    /**
+     * Write an op code than a byte of data to the decoder
+     * @param op_code the op code to define the register and the operation
+     * @param data the data to be transmitted
+     * @return 0 if success, -1 if failed
+     */
+    int write_byte(unsigned char op_code, unsigned char data);
+
+    /**
+     * Write an op code than wait for the decoder to read byte data from the SPI bus
+     * @param op_code the op code to define the register and the operation
+     * @return a byte transmitted by the decoder
+     */
+    unsigned char read_byte(unsigned char op_code);
+
+    /**
+     * Set #SS to high
+     * @return 0 if success, -1 if failed
+     */
+    int _ss_high();
+
+    /**
+     * Set #SS to low
+     * @return 0 if success, -1 if failed
+     */
+    int _ss_low();
+
+    /**
+     * Clear contents of CNTR register
+     */
+    int reset_count();
+};
+
+
+/*
+ * MDR0 configuration data - the configuration byte is formed with
+ * single segments taken from each group and ORing all together.
+ */
+
+//Count modes
+#define NQUAD 0x00          //non-quadrature mode
+#define QUADRX1 0x01        //X1 quadrature mode
+#define QUADRX2 0x02        //X2 quadrature mode
+#define QUADRX4 0x03        //X4 quadrature mode
+
+//Running modes
+#define FREE_RUN 0x00
+#define SINGE_CYCLE 0x04
+#define RANGE_LIMIT 0x08
+#define MODULO_N 0x0C
+
+//Index modes
+#define DISABLE_INDX 0x00   //index_disabled
+#define INDX_LOADC 0x10     //index_load_CNTR
+#define INDX_RESETC 0x20    //index_rest_CNTR
+#define INDX_LOADO 0x30     //index_load_OL
+#define ASYNCH_INDX 0x00    //asynchronous index
+#define SYNCH_INDX 0x80     //synchronous index
+
+//Clock filter modes
+#define FILTER_1 0x00       //filter clock frequncy division factor 1
+#define FILTER_2 0x80       //filter clock frequncy division factor 2
+
+/*
+ * MDR1 configuration data; any of these
+ * data segments can be ORed together
+ */
+
+//Flag modes
+#define NO_FLAGS 0x00       //all flags disabled
+#define IDX_FLAG 0x10       //IDX flag
+#define CMP_FLAG 0x20       //CMP flag
+#define BW_FLAG 0x40        //BW flag
+#define CY_FLAG 0x80        //CY flag
+
+//1 to 4 bytes data-width
+#define BYTE_4 0x00         //four byte mode
+#define BYTE_3 0x01         //three byte mode
+#define BYTE_2 0x02         //two byte mode
+#define BYTE_1 0x03         //one byte mode
+//Enable/disable counter
+#define EN_CNTR 0x00        //counting enabled
+#define DIS_CNTR 0x04       //counting disabled
+
+/*
+ * LS7366R op-code list
+ */
+
+#define CLR_MDR0 0x08
+#define CLR_MDR1 0x10
+#define CLR_CNTR 0x20
+#define CLR_STR 0x30
+#define READ_MDR0 0x48
+#define READ_MDR1 0x50
+#define READ_CNTR 0x60
+#define READ_OTR 0x68
+#define READ_STR 0x70
+#define WRITE_MDR1 0x90
+#define WRITE_MDR0 0x88
+#define WRITE_DTR 0x98
+#define LOAD_CNTR 0xE0
+#define LOAD_OTR 0xE4
+
+#define SPI_FREQUENCY 5000000
+
+#endif //QUANSERCONTROLLERSHIELD_DECODER_H
