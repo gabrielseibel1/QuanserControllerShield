831192465da4b8f710c7c84fcb4209568e6e69ff
Gabriel Seibel <gabrielseibel1@gmail.com>
2018-07-04 09:14:29 -0300

    Fix cross compiling

--- include/galileo2io.h
+++ include/galileo2io.h
@@ -27,17 +27,8 @@
 #ifndef GALILEO2IO_H
 #define GALILEO2IO_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 extern char *pgets(char *s, int size, const char path[]);
 
 extern int pputs(const char path[], const char s[]);
-
-#ifdef __cplusplus
-};
-#endif
 
 #endif
