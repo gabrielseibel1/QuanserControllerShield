98f492c9b1d287d9bad90f056e620ce9aa4ad925
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2018-07-03 22:17:05 -0300

    Add library test file

--- test/test.cpp
+++ test/test.cpp
@@ -2,10 +2,15 @@
  * @file Test file
  */
 
+#include <cstdio>
+#include "../include/QuanserControllerShield.h"
+
 /**
  * Script for testing the quanser controller shield
  * @return 0 if succeeded, -1 if not
  */
 int main(int argc, char *argv[]) {
-
+    QuanserControllerShield *quanserControllerShield = new QuanserControllerShield();
+    float radians = quanserControllerShield->decoder->get_position_radians();
+    printf("Hello quanser. Radians %f\n", radians);
 }
